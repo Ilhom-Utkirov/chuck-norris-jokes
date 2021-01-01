3187ca7ae04180cfa7298189171d3387aa83813d
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-01 19:40:10 +0500

    added Xml configuration and deleted @Bean configuration

--- src/main/java/com/example/demo/config/ChuckConfiguration.java
+++ src/main/java/com/example/demo/config/ChuckConfiguration.java
@@ -1,15 +1,13 @@
 package com.example.demo.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
 
-    @Bean
-    public ChuckNorrisQuotes chuckNorrisQuotes (){
+   // @Bean
+   public ChuckNorrisQuotes chuckNorrisQuotes (){
         return new ChuckNorrisQuotes();
-    }
+   }
 }
